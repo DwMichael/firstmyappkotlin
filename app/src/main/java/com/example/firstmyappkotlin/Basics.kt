f6c2259293733aebe24175906c1eb4d520f0787d
RABBITV23 <Legiony234@tlen.pl>
2023-02-25 10:53:31 +0100

    multiplication function diffrent style of writing it

--- app/src/main/java/com/example/firstmyappkotlin/Basics.kt
+++ app/src/main/java/com/example/firstmyappkotlin/Basics.kt
@@ -2,11 +2,13 @@ package com.example.firstmyappkotlin
 
 fun main()
 {
-print(multiplicationFunction(9,3))
-
+print("${multiplicationFunction(9,3)} <- Pierwsza funkcja \n")
+print("${multiplicationFunction2(8,8)} <- Druga Funkcja \n"   )
 }
 
 fun multiplicationFunction(a: Int , b: Int): Int
 {
     return a * b
 }
+
+fun multiplicationFunction2(a: Int , b: Int): Int =  a * b
