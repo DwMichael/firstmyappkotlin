2fecd082c52e004c42a65ad6936ec97828ee21b3
RABBITV23 <Legiony234@tlen.pl>
2023-03-16 19:59:10 +0100

    CLASSES AND OBJECTS

--- app/src/main/java/com/example/firstmyappkotlin/Basics.kt
+++ app/src/main/java/com/example/firstmyappkotlin/Basics.kt
@@ -2,6 +2,7 @@ package com.example.firstmyappkotlin
 
 fun main()
 {
+    Person("Robert","Jakubowicz")
     var myName : String  = " Michał";
     var flaost :Float = 23.23F
     var numberD : Double = 23.2223232
@@ -14,6 +15,11 @@ fun main()
     println("Hello " + myName +"\n")
     print("${multiplicationFunction(9,3)} <- Pierwsza funkcja \n")
     print("${multiplicationFunction2(8,8)} <- Druga Funkcja \n"   )
+
+    for( i in 1..10)
+    {
+        print("$i \n");
+    }
 }
 
 fun multiplicationFunction(a: Int , b: Int): Int
@@ -21,4 +27,11 @@ fun multiplicationFunction(a: Int , b: Int): Int
     return a * b
 }
 
+
+class Person constructor(firstName: String , LastNeme:String)
+{
+    init {
+        print("welcome $firstName travler with last name $LastNeme \n")
+    }
+}
 fun multiplicationFunction2(a: Int , b: Int): Int =  a * b
